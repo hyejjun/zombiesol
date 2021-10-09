ef7ac03a6126411e8f6f14be44bf11fb2a7e3464
hyejjun <hjlee9725@gmail.com>
2021-10-09 13:32:50 +0900

    2-4

---
+++ 2-4.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        // 이 부분
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
+
+/*
+    require(A == B);
+    return "true";
+
+    참인지 아닌지 구분할 수 있는 함수
+
+    여기서는 count = 0 인지 확인한다. 처음에만 호출할 수 있게
+    
+*/
